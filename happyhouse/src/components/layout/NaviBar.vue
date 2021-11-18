5e4ba703dab1087f5710840277eccebbb2607856
sdi1358_ <sdi1358_@naver.com>
2021-11-18 19:15:16 +0900

    jsp -> vue 변경 중

--- happyhouse/src/components/layout/NaviBar.vue
+++ happyhouse/src/components/layout/NaviBar.vue
@@ -16,11 +16,6 @@
       <nav class="nav-menu d-none d-lg-block">
         <!-- 네비게이션바 -->
         <ul>
-          <!-- <li><a href="#portfolio">Portfolio</a></li> -->
-          <!-- <li>
-            <router-link :to="{ name: 'Home' }" class="link"> Home</router-link>
-          </li> -->
-
           <li>
             <router-link :to="{ name: 'Introduce' }" class="link"
               ><b-icon icon="emoji-wink" font-scale="1"></b-icon> 사이트
@@ -34,11 +29,7 @@
               주택거래정보</router-link
             >
           </li>
-          <!-- <li>
-            <router-link :to="{ name: 'Four' }" class="link"
-              ><b-icon icon="calculator" font-scale="1"></b-icon> 주택거래정보
-            </router-link>
-          </li> -->
+
           <li>
             <a href="http://localhost:80/category"> 상권정보 </a>
             <!-- <router-link :to="{ name: 'Four' }" class="link"
@@ -51,30 +42,50 @@
             </router-link>
           </li>
 
-          <!-- <c:if test="${!empty userinfo}">
-            <c:if test="${userinfo.id eq 'ssafy'}">
-              <li id="userList">
-                <a href="${root}/user/userList">회원목록</a>
-              </li>
-            </c:if>
-            <li id="myPage"><a href="${root}/user/user_info">마이페이지</a></li>
-            <li id="logOut"><a href="${root}/user/logout">로그아웃</a></li>
-          </c:if>
-          <c:if test="${empty userinfo}">
-            <li id="logIn">
-              <a href="#loginModal" data-toggle="modal">로그인</a>
-            </li>
-          </c:if> -->
-          <!-- <li>
-            <router-link :to="{ name: 'Login' }" class="link"
-              ><b-icon icon="calculator" font-scale="1"></b-icon> 로그인
-            </router-link>
-          </li>
           <li>
-            <router-link :to="{ name: 'Sign' }" class="link"
-              ><b-icon icon="calculator" font-scale="1"></b-icon> 회원가입
-            </router-link>
-          </li> -->
+            <b-navbar-nav class="ml-auto" v-if="userInfo">
+              <b-nav-item class="align-self-center"
+                ><b-avatar
+                  variant="primary"
+                  v-text="
+                    userInfo ? userInfo.userid.charAt(0).toUpperCase() : ''
+                  "
+                ></b-avatar
+                >{{ userInfo.username }}({{ userInfo.userid }})님
+                환영합니다.</b-nav-item
+              >
+              <b-nav-item class="align-self-center"
+                ><router-link
+                  :to="{ name: 'MyPage' }"
+                  class="link align-self-center"
+                  >내정보보기</router-link
+                ></b-nav-item
+              >
+              <b-nav-item
+                class="link align-self-center"
+                @click.prevent="onClickLogout"
+                >로그아웃</b-nav-item
+              >
+            </b-navbar-nav>
+            <b-navbar-nav class="ml-auto" v-else>
+              <b-nav-item-dropdown right>
+                <template #button-content>
+                  <b-icon icon="people" font-scale="2"></b-icon>
+                </template>
+                <b-dropdown-item href="#"
+                  ><router-link :to="{ name: 'SignUp' }" class="link"
+                    ><b-icon icon="person-circle"></b-icon>
+                    회원가입</router-link
+                  ></b-dropdown-item
+                >
+                <b-dropdown-item href="#"
+                  ><router-link :to="{ name: 'SignIn' }" class="link"
+                    ><b-icon icon="key"></b-icon> 로그인</router-link
+                  ></b-dropdown-item
+                >
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </li>
         </ul>
       </nav>
       <!-- .nav-menu -->
@@ -83,8 +94,24 @@
 </template>
 
 <script>
+import { mapState, mapMutations } from "vuex";
+
+const memberStore = "memberStore";
+
 export default {
   name: "NaviBar",
+  computed: {
+    ...mapState(memberStore, ["isLogin", "userInfo"]),
+  },
+  methods: {
+    ...mapMutations(memberStore, ["SET_IS_LOGIN", "SET_USER_INFO"]),
+    onClickLogout() {
+      this.SET_IS_LOGIN(false);
+      this.SET_USER_INFO(null);
+      sessionStorage.removeItem("access-token");
+      if (this.$route.path != "/") this.$router.push({ name: "Home" });
+    },
+  },
 };
 </script>
 
